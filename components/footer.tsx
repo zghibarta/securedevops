42dc0860394a9ff0b5fbd7ed673ffefc08a94958
zghibarta <zghibarta@gmail.com>
2025-04-17 00:51:28 +0300

    footer copiryght

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="border-t py-6 md:py-0">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row px-4 md:px-6">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          &amp;copy; {new Date().getFullYear()}{" "}
+          © {new Date().getFullYear()}{" "}
           Securedevops - platformă informativ-educațională despre securitatea în DevOps
         </p>
         <div className="flex gap-4">
