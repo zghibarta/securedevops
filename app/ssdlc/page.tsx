8be01d29880aee7028affd284de7160bb8d34b6c
zghibarta <zghibarta@gmail.com>
2025-04-25 14:30:36 +0000

    ssdlc page

--- app/ssdlc/page.tsx
+++ app/ssdlc/page.tsx
@@ -59,7 +59,7 @@ export default function SSDLCPage() {
               </p> */}
             </div>
 
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
               {phases.map((phase: any) => (
                 <Card key={phase.id} className="flex flex-col h-full">
                   <CardHeader>
